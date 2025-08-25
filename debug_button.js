2d1d0725b173d0309efc1fde9be8fe1623134855
NGO SI TIEN <57280083+sitien173@users.noreply.github.com>
2025-08-25 16:34:14 +0700

    Create debug_button.js

---
+++ debug_button.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         DebugButton
+// @version      1.0.0
+// @description  add debug button to navigate to adminportal
+// @author       https://github.com/sitien173
+// @match        *://*/eidv/personMatch*
+// @match        *://*/verification*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @connect      auto-completed.sitienbmt.workers.dev
+// @downloadURL https://update.greasyfork.org/scripts/546750/AutoComplete.user.js
+// @updateURL https://update.greasyfork.org/scripts/546750/AutoComplete.meta.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
