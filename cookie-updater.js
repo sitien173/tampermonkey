ed3c313c8131a57a7201c6e62316fafa1bae03d1
Tien Ngo <sitienbmt@gmail.com>
2025-10-01 15:08:15 +0700

    up ver

--- cookie-updater.js
+++ cookie-updater.js
@@ -2,7 +2,7 @@
 // @name         Cookie Updater
 // @description  Automatically fetch and update udemy cookies automatically
 // @namespace https://greasyfork.org/users/1508709
-// @version      1.1.3
+// @version      1.2.1
 // @author       https://github.com/sitien173
 // @match        *://*.itauchile.udemy.com/*
 // @grant        GM_setValue
